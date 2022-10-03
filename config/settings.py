0025099be641da11b099ad35f2e4d970d76735d1
Kenno-Warise <wariken0523@gmail.com>
2022-10-03 13:02:22 +0900

    UI作成（店舗側）

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ログイン認証
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'reserve_list'
