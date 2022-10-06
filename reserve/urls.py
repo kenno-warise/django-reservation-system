2f4f619881b602e2a0f621627bfee9d6d80c5e04
Kenno-Warise <wariken0523@gmail.com>
2022-10-06 09:55:19 +0900

    既存の設定内容をshopテーブルから取得し、表示

--- reserve/urls.py
+++ reserve/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
         path('login/', views.Login.as_view(), name='login'),
         path('logout/', views.Logout.as_view(), name='logout'),
         path('reserve_list/', views.reserve_list, name='reserve_list'),
-        path('setting/', views.setting, name='setting'),
+        path('setting/<int:id>', views.setting, name='setting'),
 ]
